916fe56aec7879909ac7c689b566564b4124c740
Jairo Galeas <jairosindj@yahoo.com>
2017-04-13 14:45:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ if (isset($_POST["registrar"])) {
 	<head>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<meta charset="utf-8" />
-		<title>Login  - Sinstema de Usuarios</title>
+		<title>Login  - Sistema de Usuarios</title>
 
 		<meta name="description" content="User login page" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
